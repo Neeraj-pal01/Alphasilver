16cda0361ac245686cf0883ff8c9b05a25a41918
Neeraj pal <ansupal01@gmail.com>
2025-01-04 01:07:33 +0530

    now every thing is good app is succecfully running except footer home page is ready

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,12 +2,8 @@ import React from 'react'
 
 export default function Footer() {
   return (
-<<<<<<< HEAD
     <>
       
     </>
-=======
-    <div>Footer</div>
->>>>>>> 3d38f9bb495b9e69eca35182f50ab4ec68352d58
   )
 }
